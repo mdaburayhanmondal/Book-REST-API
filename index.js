42224bd999cce5ace626ce8c8e779839675be7f1
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-02-05 21:43:19 +0600

    implemented compression

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ quiet: true });
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const compression = require('compression');
 const booksRoute = require('./src/routes/booksRoute.js');
 const port = process.env.PORT || 3000;
 
@@ -13,6 +14,9 @@ app.use(
   }),
 );
 
+app.use(compression());
+// // without this, size of demoData is around 40kb, but with it the size is around 400b
+
 // view all books
 app.use('/books', booksRoute);
 
@@ -20,6 +24,8 @@ app.get('/', (req, res) => {
   res.json({
     allBooks: `Visit /books to view all books`,
     specificBook: 'Visit /books/id to view specific book',
+    blankSpace: ' '.repeat(200),
+    demoData: 'Checking compression'.repeat(2000),
   });
 });
 
